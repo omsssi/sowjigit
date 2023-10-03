3c9703bb21821b49fcbcbee6f1cbf875ce5e14d5
omsssi <vamsi.k1842@gmail.com>
2023-10-03 22:59:48 +0530

    oye check my code and commit process is crct r not

---
+++ code.js
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SOWJI dream houses</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>SOWJI dream houses</h1>
+    </header>
+    <style>
+        * {
+        background-color: grey;
+      }
+      * p span {
+        background-color: yellow;
+      }
+
+    </style>
+
+    <button type="button"
+    onclick="document.getElementById('demo').innerHTML = Date()">
+    Date and Time.</button>
+    <p id="demo"></p>
+    
+    <section class="listing">
+        <img src="C:\Users\vamsi\OneDrive\Desktop\new.jpg" alt="Property 1">
+        <h2>Beautiful Home with Garden</h2>
+        <p>Price: $300,000</p>
+        <p>Location: hyderbad, siddipet</p>
+        <a href="C:\Users\vamsi\OneDrive\Desktop\HTML\.vscode\property1_details.html">View Details</a>
+    </section>
+
+    <section class="listing">
+        <img src="C:\Users\vamsi\OneDrive\Desktop\new2.jpg" alt="Property 2">
+        <h2>Spacious Apartment in Downtown</h2>
+        <p>Price: $500,000</p>
+        <p>Location: hyderabad, patancheruv</p>
+        <a href="C:\Users\vamsi\OneDrive\Desktop\HTML\.vscode\property2_details.html">View Details</a>
+    </section>
+
+    <section class="listing">
+        <img src="C:\Users\vamsi\OneDrive\Desktop\new1.jpg" alt="Property 3">
+        <h2>Good Apartment with parking</h2>
+        <p>Price: $250,000</p>
+        <p>Location: andhra, rajam</p>
+        <a href="C:\Users\vamsi\OneDrive\Desktop\HTML\.vscode\property3_details.html">View Details</a>
+    </section>
+
+    <!-- Add more listings as needed -->
+
+    <footer>
+        <p>&copy; 2023 Real Estate Platform </p>
+    </footer>
+</body>
+</html>
